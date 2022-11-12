9fbce0e3ec4a062da9076f2565999904ce3bddb5
nikslav <nikslav7@gmail.com>
2022-11-12 19:36:01 +0200

    a

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-checkIfNotMain();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
@@ -18,5 +17,5 @@ reportWebVitals();
 
 
 function checkIfNotMain(){
-  if (window.location.pathname !== '/ArchiTrollingWebsite/') window.location.pathname    = '/ArchiTrollingWebsite/';
+  if (window.location.pathname !== '/ArchiTrollingWebsite') window.location.pathname    = '/ArchiTrollingWebsite';
 }
